db7d5faf7ad2fea076fddb150f694a289d3b33f5
kosayoda <git@siek.dev>
2023-03-26 03:33:01 -0400

    WIP editor highlighting.

--- frontend/src/app/editor/editor.component.ts
+++ frontend/src/app/editor/editor.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
+import { Doc, TextMarker } from 'codemirror';
 
 import * as Module from '../../assets/pkg/compiler';
 import { tick } from './Tick';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
 
 @Component({
   selector: 'app-editor',
@@ -16,6 +18,7 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   ]
 })
 export class EditorComponent implements OnInit {
+  @ViewChild('codeEditor', { static: false }) codeEditor?: CodemirrorComponent;
 
   content: string = `set sides 10
 set turn_amount 360
@@ -43,13 +46,24 @@ repeat sides:
   showError: boolean = false;
   errorMessage: SafeHtml = `This is a multiline error message\n\n  Line 15 column 89: unrecognized token "among"\n\nFix or blah, blah, blah`
 
+  lineHighlight: null | number = null;
+  ticks: any[] = [];
+  tickIndex = 0;
+  showRun: boolean = false;
+  marker: null | TextMarker = null;
+
   lineFormatter(line: string): string {
     return `${line} | `;
   }
 
   submitClicked() {
+  }
+
+  runClicked() {
     try {
-      let result: tick[] = this.rust?.compile_and_execute(this.content, { x: 500, y: 500 }, 1000, 1000);
+      this.showRun = false;
+      this.ticks = this.rust?.compile_and_execute(this.content, { x: 500, y: 500 }, 1000, 1000);
+      this.showRun = true;
       this.closeErrorMessage();
     }
     catch (error: any) {
@@ -57,8 +71,39 @@ repeat sides:
     }
   }
 
-  runClicked() {
-    this.raiseError(this.content);
+
+  step() {
+    if (this.tickIndex == this.ticks.length) {
+      this.showRun = false;
+      return;
+    }
+
+    console.log(this.ticks);
+    let tick = this.ticks[this.tickIndex];
+    this.lineHighlight = tick.Tick.line || tick.Changed.line || tick.Invalid.line;
+    if (this.lineHighlight) {
+      const { doc } = this;
+      if (doc) {
+        // Clear markers
+        this.marker?.clear();
+        this.marker = doc.markText({line: this.lineHighlight, ch: 0}, {line: this.lineHighlight, ch: 2}, {
+          className: "current-inst",
+        });
+        console.log(this.marker);
+      }
+    }
+    this.tickIndex++;
+    //   const highlightLines = (editor, start, end) => {
+    //   const from: {line: start, ch: 0};
+    //   const to: {line: end, ch: MAX_LINE_LENGTH};
+    // }
+  }
+
+  stepAll() {
+  }
+
+  get doc() {
+    return (this.codeEditor?.codeMirror as any) as Doc;
   }
 
   closeErrorMessage() {
